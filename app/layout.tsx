25b7eb6b4dcd6e08a99599fdb386c102f84e0621
Nova <nova940116@gmail.com>
2023-11-30 06:07:04 +0900

    ..

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Noto_Sans_KR } from 'next/font/google'
+import { ThemeProvider } from "@/app/components/theme-provider"
+import { ModeToggle } from "@/app/components/mode-toggle"
 import './globals.css'
+import Link from 'next/link'
 
-const inter = Inter({ subsets: ['latin'] })
+const notoSansKR = Noto_Sans_KR({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +19,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-      <body className={inter.className}>{children}</body>
+      <body className={notoSansKR.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <div className="max-w-2xl mx-auto py-10 px-4">
+            <header className="flex py-5">
+              <Link href="/">
+                <h1>노바로그</h1>
+              </Link>
+              <nav className="ml-5">
+                <Link href="" className="px-2">소개</Link>
+                <Link href="" className="px-2">문서</Link>
+              </nav>
+            </header>
+            {children}
+          </div>
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,12 @@ import Link from 'next/link'
 export default async function Home() {
   const posts = await getPosts()
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       {posts.map((post) => 
         <Link href={`/post/${post?.slug}`} key={post?.slug}>
           <article>
             <h2>{post?.title}</h2>
+            <p>{post?.description}</p>
           </article>  
         </Link>
       )}
