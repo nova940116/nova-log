f91d31d4d307a3a2ff2e5e6b72068e32c5680f42
Nova <nova940116@gmail.com>
2023-11-30 22:08:37 +0900

    ?

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import { MDXRemote } from 'next-mdx-remote/rsc'
-import { getPost } from '@/app/utils'
+import { getPost, getPosts } from '@/app/utils'
 import { notFound } from 'next/navigation' 
 import { Code } from "bright"
 
@@ -9,9 +9,17 @@ type Params = {
   }
 }
 
-export default async function Post(params: Params) {
+export async function generateStaticParams() {
+  const posts = await getPosts()
+ 
+  return posts.map((post) => ({
+    slug: post.slug,
+  }))
+}
+
+export default async function Post({ params }: any) {
   // MDX text - can be from a local file, database, CMS, fetch, anywhere...
-  const post = await getPost(params.params.slug)
+  const post = await getPost(params.slug)
   if (!post) return notFound()
   const components = {
     pre: (props: any) => (
@@ -24,7 +32,7 @@ export default async function Post(params: Params) {
     <main>
       <h1 className='text-2xl'>{post.title}</h1>
       <p className='py-2'>{post.date}</p>
-      {/* <MDXRemote source={post.body} components={components} /> */}
+      <MDXRemote source={post.body} components={components} />
     </main>
   )
 }
