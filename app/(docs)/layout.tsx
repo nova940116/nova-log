2484fd9131a7129495a28d3f6f36eb67d8673878
Nova <nova940116@gmail.com>
2023-12-03 23:44:59 +0900

    ff

--- app/(docs)/layout.tsx
+++ app/(docs)/layout.tsx
@@ -1,16 +1,30 @@
 import Link from "next/link"
+import { getDocs } from "@/app/utils"
 
 type DocsLayoutProps = {
   children: React.ReactNode
 }
 
-export default function DocsLayout({ children }: DocsLayoutProps) {
+export async function generateStaticParams() {
+  const docs = await getDocs()
+ 
+  return docs.map((doc) => ({
+    slug: doc.slug,
+  }))
+}
+
+export default async function DocsLayout({ children }: DocsLayoutProps) {
+  const docs = await getDocs()
+  console.log(docs, '@?')
   return (
-    <main>
+    <main className="flex">
       <section>
         <nav className="flex flex-col">
-          <Link href="/doc/javascript">t</Link>
-          <Link href="/doc/vue">e</Link>
+        {docs.map((doc) => 
+          <>
+            <Link href={`/doc/${doc.slug}`}>{doc.title}</Link>
+          </>
+        )}
         </nav>
       </section>
       <section className="max-w-2xl mx-auto px-4 py-10">
