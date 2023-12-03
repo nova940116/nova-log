635f8ee90aa1033aee4f0a6c2b032d63db98a791
Nova <nova940116@gmail.com>
2023-12-03 22:54:54 +0900

    ..

---
+++ app/(docs)/doc/[slug]/page.tsx
@@ -0,0 +1,37 @@
+import { MDXRemote } from 'next-mdx-remote/rsc'
+import { getDoc, getDocs } from '@/app/utils'
+import { notFound } from 'next/navigation' 
+import { Code } from "bright"
+
+type Params = {
+  params: {
+    slug: string
+  }
+}
+
+export async function generateStaticParams() {
+  const docs = await getDocs()
+
+  return docs.map((doc) => ({
+    slug: doc.slug,
+  }))
+}
+
+export default async function Doc({ params }: Params) {
+  const doc = await getDoc(params.slug)
+  if (!doc) return notFound()
+  const components = {
+    pre: (props: any) => (
+      <Code {...props}>
+        {props.children}
+      </Code>
+    )
+  }
+  return (
+    <main>
+      <h1 className='text-2xl font-bold'>{doc.title}</h1>
+      <p className='py-2'>{doc.description}</p>
+      <MDXRemote source={doc.body} components={components} />
+    </main>
+  )
+}
