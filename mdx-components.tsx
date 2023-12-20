5e942399271fde4857df0dd818d795d6a6f4695d
Nova <nova940116@gmail.com>
2023-12-21 06:19:59 +0900

    ..

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,6 +1,7 @@
 import type { MDXComponents } from 'mdx/types'
 import { Code } from "bright" 
+import Image from 'next/image'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return { ...components, pre: Code }
+  return { ...components, pre: Code, Image: (props) => <Image {...props} /> }
 }
